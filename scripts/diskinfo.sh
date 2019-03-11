fda09f4e8ddd1585e833f083e4b813eced78d30c
rgaag <rgaag@os3.nl>
2019-03-11 16:57:49 +0100

    modified:   scripts/diskinfo.sh

--- scripts/diskinfo.sh
+++ scripts/diskinfo.sh
@@ -5,12 +5,16 @@ cd /forensics
 version="0.0.1 (beta)"
 workdir=$(pwd)
 username=$(whoami)
+donor="\e[31mUnknown!\e[0m"
+target="\e[31mUnknown!\e[0m"
 clear
 
 basic=" \n
  Version        :    $version \n
  Work Directory :    $workdir \n
  Username       :    $username \n
+ Acquired Disk  :    $donor \n
+ Police Disk    :    $target \n
 \n
 ============================================= \n
 |     Remote Forensic Imager - DiskInfo     | \n
@@ -45,3 +49,8 @@ else
 	echo "New mounted device found!"
 	echo "Device is mounted at $donor ."
 fi
+
+
+clear
+echo $basic
+lsblk $donor
